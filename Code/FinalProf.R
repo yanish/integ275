f11df2a3827b5762b3842c25fcf4dc8c13dfc301
Yanish <Yanish@MacBook-Pro-2.local>
2016-04-05 15:48:53 -0400

    test2

--- Code/FinalProf.R
+++ Code/FinalProf.R
@@ -8,6 +8,22 @@ library(ggplot2)
 # Set the working directory to the folder containing the example data
 setwd("/Users/Yanish/Documents/winter_2016/Integ_275/final") 
 
-dat <- read.csv("/Users/Yanish/Documents/winter_2016/Integ_275/final/Data/FinalAdjacencyMatrix.csv")
 
-head(dat)
+dat <- read.csv("/Users/Yanish/Documents/winter_2016/Integ_275/final/Data/FinalAdjacencyMatrix.csv")
+matrix <- as.matrix(read.csv("/Users/Yanish/Documents/winter_2016/Integ_275/final/Data/FinalAdjacencyMatrix.csv", sep=",", header=TRUE, row.names=1))
+network1 <- graph.adjacency(matrix,weighted=TRUE)
+
+network <- delete.vertices(network1, which(degree(network1) < 1))
+network <- simplify(network, remove.multiple = TRUE, remove.loops = TRUE)
+
+head(matrix)
+
+set.seed(875)
+par(mar=c(0,0,0,0)+.1)
+l1 = layout.kamada.kawai(network)
+l2 = layout.fruchterman.reingold(network)
+plot(network, layout = l1, 
+     vertex.label = NA, 
+     vertex.color = "orange", 
+     vertex.size = 3,
+     edge.arrow.size = 0.3)
